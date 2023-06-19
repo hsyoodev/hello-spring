352a37d01313604737193b45a262d010ae4d3885
hsyoodev <hsyoodev@gmail.com>
2023-06-19 16:13:08 +0900

    Spring Bin(Component Scan)

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,16 +1,19 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.damain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-public class MemoryMemberRepository implements MemberRepository{
+@Repository
+public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
+
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
-        store.put(member.getId(),member);
+        store.put(member.getId(), member);
         return member;
     }
 
@@ -29,7 +32,7 @@ public class MemoryMemberRepository implements MemberRepository{
         return new ArrayList<>(store.values());
     }
 
-    public void clearStore(){
+    public void clearStore() {
         store.clear();
     }
 }
