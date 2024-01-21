8ac5751e01841ab733a85f131d1a50fa47b1da7c
hsyoodev <hsyoodev@gmail.com>
2024-01-21 16:12:28 +0900

    #1 싱글톤 컨테이너

--- src/main/java/com/hello/core/AppConfig.java
+++ src/main/java/com/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package com.hello.core;
 
 import com.hello.core.discount.DiscountPolicy;
-import com.hello.core.discount.FixDiscountPolicy;
 import com.hello.core.discount.RateDiscountPolicy;
 import com.hello.core.member.MemberRepository;
 import com.hello.core.member.MemberService;
@@ -16,11 +15,15 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Bean
     public MemberService memberService() {
+        //1번
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService() {
+        //1번
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
@@ -28,6 +31,8 @@ public class AppConfig {
 
     @Bean
     public MemberRepository memberRepository() {
+        //2번? 3번?
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
