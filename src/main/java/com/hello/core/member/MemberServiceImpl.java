5122619f184481700d96a7cb8fa2e97ba5425392
hsyoodev <hsyoodev@gmail.com>
2024-01-21 15:27:21 +0900

    #1 스프링 핵심 원리 이해2 - 객체 지향 원리 적용
    * 좋은 객체 지향 설계의 5가지 원칙의 적용

--- src/main/java/com/hello/core/member/MemberServiceImpl.java
+++ src/main/java/com/hello/core/member/MemberServiceImpl.java
@@ -1,12 +1,16 @@
 package com.hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
-    private final MemberRepository memberRepository = new
-            MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     public void join(Member member) {
         memberRepository.save(member);
     }
+
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
--- src/test/java/com/hello/core/member/MemberServiceTest.java
+++ src/test/java/com/hello/core/member/MemberServiceTest.java
@@ -1,12 +1,23 @@
 package com.hello.core.member;
 
+import com.hello.core.AppConfig;
+import com.hello.core.order.OrderService;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void join(){
