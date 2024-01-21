a67c4d77785f5ab264f2debdb2823a4e3f541282
hsyoodev <hsyoodev@gmail.com>
2024-01-21 16:41:43 +0900

    #1 컴포넌트 스캔

--- src/main/java/com/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/com/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,12 @@ package com.hello.core.discount;
 
 import com.hello.core.member.Grade;
 import com.hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10; //10% 할인
+
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
