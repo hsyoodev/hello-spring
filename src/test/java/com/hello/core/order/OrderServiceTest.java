901f8e22cbceb1e967a1d877f36e3f1272fbe54d
hsyoodev <hsyoodev@gmail.com>
2024-01-21 15:27:21 +0900

    #1 스프링 핵심 원리 이해2 - 객체 지향 원리 적용
    * 좋은 객체 지향 설계의 5가지 원칙의 적용

--- src/test/java/com/hello/core/order/OrderServiceTest.java
+++ src/test/java/com/hello/core/order/OrderServiceTest.java
@@ -1,17 +1,26 @@
 package com.hello.core.order;
 
+import com.hello.core.AppConfig;
 import com.hello.core.member.Grade;
 import com.hello.core.member.Member;
 import com.hello.core.member.MemberService;
 import com.hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
