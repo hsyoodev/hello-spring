8b6aefef4c6764eb7b2031ba0886edea683c24f1
hsyoodev <hsyoodev@gmail.com>
2024-01-24 21:27:06 +0900

    #1 의존관계 자동 주입
    * 다양한 의존관계 주입 방법
    * 옵션 처리
    * 생성자 주입을 선택해라!

--- src/test/java/com/hello/core/AutoAppConfigTest.java
+++ src/test/java/com/hello/core/AutoAppConfigTest.java
@@ -1,6 +1,8 @@
 package com.hello.core;
 
+import com.hello.core.member.MemberRepository;
 import com.hello.core.member.MemberService;
+import com.hello.core.order.OrderServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,7 +15,11 @@ public class AutoAppConfigTest {
         ApplicationContext ac = new
                 AnnotationConfigApplicationContext(AutoAppConfig.class);
         MemberService memberService = ac.getBean(MemberService.class);
-        
+
         assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
