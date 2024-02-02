8ebb113dbd71f38cd0b2aa738727c5be6e32b1c6
hsyoodev <hsyoodev@gmail.com>
2024-02-02 20:56:58 +0900

    #1 의존관계 자동 주입
    * 생성자 주입을 선택해라!

--- src/test/java/com/hello/core/order/OrderServiceImplTest.java
+++ src/test/java/com/hello/core/order/OrderServiceImplTest.java
@@ -1,13 +1,17 @@
 package com.hello.core.order;
 
 import com.hello.core.discount.FixDiscountPolicy;
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
 import com.hello.core.member.MemoryMemberRepository;
 import org.junit.jupiter.api.Test;
 
 class OrderServiceImplTest {
     @Test
     void createOrder() {
-        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
         orderService.createOrder(1L, "itemA", 10000);
     }
 }
