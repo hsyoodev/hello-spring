ca44728f2b7ddf522ea4021d44438f44b0dce6e1
hsyoodev <hsyoodev@gmail.com>
2024-07-02 23:15:45 +0900

    #3 단순하고 실용적인 컨트롤러 - v4

--- src/main/java/com/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ src/main/java/com/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -38,8 +38,8 @@ public class FrontControllerServletV4 extends HttpServlet {
         Map<String, Object> model = new HashMap<>();
 
         String viewName = controller.process(paramMap, model);
-
         MyView view = viewResolver(viewName);
+        
         view.render(model, request, response);
     }
 
