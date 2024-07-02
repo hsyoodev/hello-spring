03c7b472916e7535bdd782b2af1aa37222de7c7c
hsyoodev <hsyoodev@gmail.com>
2024-07-02 23:11:24 +0900

    #3 유연한 컨트롤러1 - v5

---
+++ src/main/java/com/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -0,0 +1,33 @@
+package com.hello.servlet.web.frontcontroller.v5.adapter;
+
+import com.hello.servlet.web.frontcontroller.ModelView;
+import com.hello.servlet.web.frontcontroller.v3.ControllerV3;
+import com.hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV3);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handle) throws ServletException, IOException {
+        ControllerV3 controller = (ControllerV3) handle;
+        Map<String, String> paramMap = createParamMap(request);
+        return controller.process(paramMap);
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
