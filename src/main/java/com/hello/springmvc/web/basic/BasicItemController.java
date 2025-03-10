48c45992fc577e792de1a426dd7a14a0a1385d18
hsyoodev <hsyoodev@icloud.com>
2025-03-10 23:29:19 +0900

    #3 상품 목록 - 타임리프

---
+++ src/main/java/com/hello/springmvc/web/basic/BasicItemController.java
@@ -0,0 +1,34 @@
+package com.hello.springmvc.web.basic;
+
+import com.hello.springmvc.domain.item.Item;
+import com.hello.springmvc.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String item(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
