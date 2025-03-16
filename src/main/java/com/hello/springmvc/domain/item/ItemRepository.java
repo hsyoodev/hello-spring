ae2e793233ba45e7fedb13f8de97f5a70c638703
hsyoodev <hsyoodev@icloud.com>
2025-03-16 12:48:35 +0900

    #3 상품 수정

--- src/main/java/com/hello/springmvc/domain/item/ItemRepository.java
+++ src/main/java/com/hello/springmvc/domain/item/ItemRepository.java
@@ -27,7 +27,7 @@ public class ItemRepository {
         return new ArrayList<>(store.values());
     }
 
-    public void upate(Long itemId, Item updateParam) {
+    public void update(Long itemId, Item updateParam) {
         Item findItem = findById(itemId);
         findItem.setItemName(updateParam.getItemName());
         findItem.setPrice(updateParam.getPrice());
