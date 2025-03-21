c7549d7a4c484854b3b2650d85a2173334cf3bda
hsyoodev <hsyoodev@icloud.com>
2025-03-21 21:05:55 +0900

    #4 유틸리티 객체와 날짜

--- src/main/java/com/hello/springmvc/basic/BasicController.java
+++ src/main/java/com/hello/springmvc/basic/BasicController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -56,6 +57,13 @@ public class BasicController {
         session.setAttribute("sessionData", "Hello Session");
 
         return "basic/basic-objects";
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+
+        return "basic/date";
     }
 
     @Component("helloBean")
