63990b524f5a9ac13fe6ad0e8bc5b6fadcc65e70
hsyoodev <hsyoodev@icloud.com>
2025-05-27 20:38:11 +0900

    #4 섹션 5.검증2-Bean Validation / Bean Validation-시작

--- src/main/java/com/hello/spring/domain/item/Item.java
+++ src/main/java/com/hello/spring/domain/item/Item.java
@@ -1,13 +1,25 @@
 package com.hello.spring.domain.item;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank(message = "공백X")
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
@@ -18,5 +30,5 @@ public class Item {
         this.price = price;
         this.quantity = quantity;
     }
-    
+
 }
