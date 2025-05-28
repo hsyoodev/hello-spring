042e1357438d2c311a8cc3726e37e4e3bfde2576
hsyoodev <hsyoodev@icloud.com>
2025-05-28 22:04:56 +0900

    #4 섹션 5.검증2-Bean Validation / Bean Validation-오브젝트 오류

--- src/main/java/com/hello/spring/domain/item/Item.java
+++ src/main/java/com/hello/spring/domain/item/Item.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요.")
 public class Item {
 
     private Long id;
