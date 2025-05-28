5aa24c223e78cc8fd5f504452ae545fe1984ba77
hsyoodev <hsyoodev@icloud.com>
2025-05-28 23:03:25 +0900

    #4 섹션 6.검증2-Bean Validation / Bean Validation-수정에 적용

--- src/main/java/com/hello/spring/domain/web/validation/ValidationItemControllerV3.java
+++ src/main/java/com/hello/spring/domain/web/validation/ValidationItemControllerV3.java
@@ -43,7 +43,7 @@ public class ValidationItemControllerV3 {
     }
 
     @PostMapping("/add")
-    public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+    public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         // 특정 필드가 아닌 복합 룰 검증
         if (item.getPrice() != null && item.getQuantity() != null) {
@@ -76,8 +76,23 @@ public class ValidationItemControllerV3 {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            return "validation/v3/editForm";
+        }
+
         itemRepository.update(itemId, item);
+
         return "redirect:/validation/v3/items/{itemId}";
     }
 
