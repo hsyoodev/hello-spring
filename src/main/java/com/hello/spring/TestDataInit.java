dc477e3a92aa55b70629a485656b835986cd8caf
hsyoodev <hsyoodev@icloud.com>
2025-06-09 22:07:44 +0900

    #4 섹션7.로그인 처리1-쿠키,세션 / 회원 가입

--- src/main/java/com/hello/spring/TestDataInit.java
+++ src/main/java/com/hello/spring/TestDataInit.java
@@ -2,6 +2,8 @@ package com.hello.spring;
 
 import com.hello.spring.domain.item.Item;
 import com.hello.spring.domain.item.ItemRepository;
+import com.hello.spring.domain.member.Member;
+import com.hello.spring.domain.member.MemberRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 테스트용 데이터 추가
@@ -19,6 +22,14 @@ public class TestDataInit {
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("테스터");
+
+        memberRepository.save(member);
     }
 
 }
