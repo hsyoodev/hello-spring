cf03ba5a2bb68e9d45cf46443f5b460f276a6264
hsyoo <hsyoodev@icloud.com>
2025-06-12 23:22:54 +0900

    #4 섹션7.로그인 처리1-쿠키,세션 / 로그인 처리하기-쿠키 사용

--- src/main/java/com/hello/spring/web/login/LoginController.java
+++ src/main/java/com/hello/spring/web/login/LoginController.java
@@ -2,6 +2,8 @@ package com.hello.spring.web.login;
 
 import com.hello.spring.domain.login.LoginService;
 import com.hello.spring.domain.member.Member;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -24,7 +26,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -37,8 +39,25 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // TODO: 로그인 성공 처리
+        // 로그인 성공 처리
+        // 쿠키에 시간 정보를 주지 않으며 세션 쿠키(브라우저 종료 시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
+
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie("memberId", null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 
 }
