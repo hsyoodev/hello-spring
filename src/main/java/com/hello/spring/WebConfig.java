9f69517b6d5e8e4beb5211483ff39957c5e1c1a7
hsyoo <hsyoodev@icloud.com>
2025-06-17 23:45:09 +0900

    #4 섹션8.로그인 처리2-필터,인터셉터 / 서블릿 필터-요청 로그

---
+++ src/main/java/com/hello/spring/WebConfig.java
@@ -0,0 +1,23 @@
+package com.hello.spring;
+
+import com.hello.spring.web.filter.LogFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+
+        return filterFilterRegistrationBean;
+    }
+    
+}
